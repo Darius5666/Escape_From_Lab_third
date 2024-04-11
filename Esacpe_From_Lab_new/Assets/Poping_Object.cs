051ef0df874ab3229cdf68a1f19ad731b873840c
Darius5666 <hreweee@gmail.com>
2024-04-12 03:59:59 +0800

    2024/4/12
    Setup

--- Esacpe_From_Lab_new/Assets/Poping_Object.cs
+++ Esacpe_From_Lab_new/Assets/Poping_Object.cs
@@ -8,29 +8,42 @@ public class Poping_Object : MonoBehaviour
     public Button Target ;
     public Image Target_Img;
     public Button Background;
+    public int Tasknum;
     private bool isClick=false;
+    public GameObject me; 
     void Start()
     {
-            Button btn=Target.GetComponent<Button>();
-            Button button=Background.GetComponent<Button>();
-            
-            btn.onClick.AddListener(ShowImage);
-            button.onClick.AddListener(GoBack);
+        
+        Button btn=Target.GetComponent<Button>();
+        Button button=Background.GetComponent<Button>();
+        Target_Img.enabled = false;
+        btn.onClick.AddListener(ShowImage);
+        button.onClick.AddListener(GoBack);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+            
     }
     void ShowImage()
     {
-        Debug.Log("Hi");
+        me.SetActive(false);
         Target_Img.enabled = true;
+            Debug.Log(isClick);
+        isClick= true;
+        
     }
     void GoBack()
     {
-
+        if (isClick)
+        {
+            me.SetActive(true);
             Target_Img.enabled = false;
+            Debug.Log(isClick);
+            isClick = false;
+        }
+        
     }
 }
