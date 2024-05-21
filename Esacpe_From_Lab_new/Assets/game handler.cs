a34fabcf6472fd76c16e134c1318e3e47a6955a8
Darius5666 <hreweee@gmail.com>
2024-05-21 13:07:19 +0800

    2024/5/21

--- Esacpe_From_Lab_new/Assets/game handler.cs
+++ Esacpe_From_Lab_new/Assets/game handler.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class gamehandler : MonoBehaviour, IPointerDownHandler{
+public class gamehandler : MonoBehaviour, IPointerDownHandler { 
+   
+
     public void OnPointerDown (PointerEventData eventData) {
         Debug.Log("OnPointerDown");
-        throw new System.NotImplementedException();
     }
-    
-        
-    }
+
+
+}
     
