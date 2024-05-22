2728b3d6823347cf1af1d882ba6290bdfe9c9a92
Sunny970118 <yi5954109@gmail.com>
2024-05-22 09:03:58 +0800

    20240522

--- Esacpe_From_Lab_new/Assets/game handler.cs
+++ Esacpe_From_Lab_new/Assets/game handler.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class gamehandler : MonoBehaviour, IPointerDownHandler { 
-   
-
-    public void OnPointerDown (PointerEventData eventData) {
+public class gamehandler : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,IDragHandler
+{
+    private RectTransform rectTransform;
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
         Debug.Log("OnPointerDown");
     }
 
-
 }
-    
