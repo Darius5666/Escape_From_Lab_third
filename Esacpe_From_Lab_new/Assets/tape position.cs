7ad944b21c954088516bff4b3523b8dc0916abac
Sunny970118 <yi5954109@gmail.com>
2024-05-28 13:04:28 +0800

    20240528

---
+++ Esacpe_From_Lab_new/Assets/tape position.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class tapeposition : MonoBehaviour
+{
+    public GameObject monitor;
+    public GameObject monitoroff;
+    // Start is called before the first frame update
+    void Start()
+    { Position 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
