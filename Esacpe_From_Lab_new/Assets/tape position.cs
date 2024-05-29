bd4a43ef4e1505716706de98bfa198efa8632e29
Sunny970118 <yi5954109@gmail.com>
2024-05-29 09:07:10 +0800

    20240529

--- Esacpe_From_Lab_new/Assets/tape position.cs
+++ Esacpe_From_Lab_new/Assets/tape position.cs
@@ -7,15 +7,26 @@ public class tapeposition : MonoBehaviour
 {
     public GameObject monitor;
     public GameObject monitoroff;
+    float PosX;
+    float PosY;
+    float PosMX;
+    float PosMY;
     // Start is called before the first frame update
     void Start()
-    { Position 
-        
+    {PosX=GetComponent<Transform>().position.x;
+     PosY=GetComponent<Transform>().position.y;
+     PosMX = monitor.GetComponent<Transform>().position.x;
+     PosMX = monitor.GetComponent<Transform>().position.y;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {if (PosMX-10<= PosX || PosX <= PosMX+10 & PosMY - 10 <= PosY || PosY <= PosMY + 10)
+        {
+            monitor.SetActive(false);
+            monitoroff.SetActive(true);
+
+        }
+     
     }
 }
